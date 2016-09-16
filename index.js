b03355ed8f2153873a1f85c1ea7f52c23a77baff
Gagge <tobias@nublar.se>
2016-09-16 12:49:53 +0200

    Using callbacks instead of promised and some tests

--- index.js
+++ index.js
@@ -1,3 +1,140 @@
 'use strict';
 
-exports.Intercom = require('./models/intercom.js');
+const	log	= require('winston'),
+	amqp	= require('amqplib/callback_api'),
+	events	= require('events');
+
+function Intercom(url) {
+	const that = this;
+	this.connected = false;
+	this.eventEmitter	= new events.EventEmitter();
+
+	amqp.connect(url, function(err, connection) {
+		if (err) {
+			log.error('larvitamintercom: Connection error: ' + err.message);
+			return;
+		}
+
+		that.connection = connection;
+		that.connected = true;
+		that.eventEmitter.emit('connected');
+	});
+};
+
+// Sending messages directly to que.
+Intercom.prototype.send = function(options, msg) {
+	const	that	= this;
+
+	this.ready(function() {
+		if (options.que === undefined)	options.que = '';
+		if (options.durable === undefined)	options.durable = false;
+		if (options.publish === undefined)	options.publish = true;
+		if (options.exchange === undefined)	options.exchange = options.que;
+
+		that.connection.createChannel(function(err, channel) {
+			if (err) {
+				log.error('larvitamintercom - send(): Channel error: ' + err.message);
+				return;
+			}
+
+			channel.assertQueue(options.que);
+			if (options.publish === true) {
+				log.info('Authoban Intercom - Sending message on que \'' + options.que + '\' and publishing on exchange \'' + options.exchange + '\': ' + msg);
+			} else {
+				log.info('Authoban Intercom - Sending message on que \'' + options.que + '\': ' + msg);
+			}
+
+			channel.sendToQueue(options.que, new Buffer(msg));
+
+			if (options.publish === true) {
+				that.publish({exchange: options.exchange}, msg);
+			}
+		});
+	});
+};
+
+// Consume messages directly from que;
+Intercom.prototype.consume = function(options, cb) {
+	const	that	= this;
+
+	this.ready(function() {
+		if (options.que === undefined)	options.que = '';
+		if (options.ack === undefined)	options.ack = false;
+
+		that.connection.createChannel(function(err, channel) {
+			if (err) {
+				log.error('larvitamintercom - consume(): Channel error: ' + err.message);
+				return;
+			}
+			channel.assertQueue(options.que);
+			log.info('Authoban Intercom - Consuming messages on que: \'' + options.que + '\'');
+			channel.consume(options.que, function(msg) {
+				cb(msg);
+			}, {noAck: options.ack});
+
+		});
+	});
+};
+
+
+// Publish messages on exchanges.
+Intercom.prototype.publish = function(options, msg) {
+	const	that	= this;
+
+	this.ready(function() {
+		if (options.exchange === undefined)	options.exchange = '';
+		if (options.type === undefined)	options.type = 'fanout';
+
+		that.connection.createChannel(function(err, channel) {
+			if (err) {
+				log.error('larvitamintercom - consume(): Channel error: ' + err.message);
+				return;
+			}
+			channel.assertExchange(options.exchange, options.type, {durable: false});
+			log.info('Authoban Intercom - Publishing message on exhange \'' +  options.exchange + '\': ' + msg.toString());
+			channel.publish(options.exchange, '', new Buffer(msg));
+		});
+	});
+};
+
+// Subscribe on messagenes of exhange.
+Intercom.prototype.subscribe = function(options, cb) {
+	const	that	= this;
+
+	this.ready(function() {
+		if (options.exchange === undefined)	options.exchange = '';
+		if (options.durable === undefined)	options.durable = false;
+		if (options.type === undefined)	options.type = 'fanout';
+		if (options.ack === undefined)	options.ack = false;
+
+		that.connection.createChannel(function(err, channel) {
+			if (err) {
+				log.error('larvitamintercom - subscribe(): Channel error: ' + err.message);
+				return;
+			}
+
+			channel.assertExchange(options.exchange, options.type, {durable: options.durable});
+
+			channel.assertQueue('', {exclusive: true}, function(err, q) {
+				log.info('Authoban Intercom - Subscribing on exchange: \'' + options.exchange + '\'');
+				channel.bindQueue(q.queue, options.exchange, '');
+				channel.consume(q.queue, function(msg) {
+					cb(msg);
+				}, {noAck: options.ack});
+			});
+		});
+	});
+};
+
+// Send "connected" event when connection ready.
+Intercom.prototype.ready = function(cb) {
+	if (this.connected) {
+		cb();
+		return;
+	}
+
+	this.eventEmitter.on('connected', cb);
+};
+
+exports.Intercom	= Intercom;
+exports.ready	= this.ready;
--- test/test.js
+++ test/test.js
@@ -1,27 +1,20 @@
 'use strict';
 
 const	assert	= require('assert'),
+	async	= require('async'),
 	log	= require('winston'),
 	fs	= require('fs');
 
-let	intercom;
+let	intercom,
+	confFile,
+	altConfFile;
 
 
 // Set up winston
 log.remove(log.transports.Console);
 
 before(function(done) {
-	let confFile;
-
-	function runAutobahnSetup(confFile) {
-		const	Intercom	= require(__dirname + '/../index.js').Intercom;
-
-		log.verbose('Autobahn config: ' + JSON.stringify(require(confFile)));
-		intercom	= new Intercom(require(confFile).default);
-
-		intercom.connection.then(done);
-	}
-
+	// Set configure file
 	if (process.argv[3] === undefined)
 		confFile = __dirname + '/../config/amqp_test.json';
 	else
@@ -30,31 +23,101 @@ before(function(done) {
 	log.verbose('Autobahn config file: "' + confFile + '"');
 
 	fs.stat(confFile, function(err) {
-		const altConfFile = __dirname + '/../config/' + confFile;
+		altConfFile = __dirname + '/../config/' + confFile;
 
 		if (err) {
 			log.info('Failed to find config file "' + confFile + '", retrying with "' + altConfFile + '"');
 
 			fs.stat(altConfFile, function(err) {
 
-				if (err)
-					assert( ! err, 'fs.stat failed: ' + err.message);
+				if (err) assert( ! err, 'fs.stat failed: ' + err.message);
+				else if ( ! err) confFile	= altConfFile;
 
-				if ( ! err)
-					runAutobahnSetup(altConfFile);
+				done();
 			});
 		} else {
-			runAutobahnSetup(confFile);
+			done();
 		}
 	});
 });
 
 
-describe('Foo', function() {
-	it('Bar', function(done) {
+describe('Send, Recieve, Publish and Subscribe', function() {
 
-		done();
+	it('Test Connection', function(done) {
+		const	Intercom	= require(__dirname + '/../index.js').Intercom,
+			intercom	= new Intercom(require(confFile).default);
 
-
+		intercom.ready(done);
 	});
+
+	// We do this to ensure intercom is just called once per session.
+	it('Test parallel connection', function(done) {
+		const	Intercom	= require(__dirname + '/../index.js').Intercom,
+			intercom	= new Intercom(require(confFile).default);
+
+		intercom.ready(done);
+	});
+
+	it('Send without publish', function(done) {
+		const	message	= 'Hello World',
+			Intercom	= require(__dirname + '/../index.js').Intercom,
+			intercom	= new Intercom(require(confFile).default);
+
+		intercom.send({que: 'testQue1', publish: false}, message);
+		done();
+	});
+
+	it('Consume', function(done) {
+		const	Intercom	= require(__dirname + '/../index.js').Intercom,
+			intercom	= new Intercom(require(confFile).default);
+
+		intercom.consume({que: 'testQue1', ack: true}, function(msg) {
+			assert.deepEqual(msg.content.toString(), 'Hello World');
+			done();
+		});
+	});
+
+
+	it('Send & publish', function(done) {
+		const	Intercom	= require(__dirname + '/../index.js').Intercom,
+			intercom	= new Intercom(require(confFile).default);
+
+		let	exchangeMessageReceived	= false;
+
+		intercom.subscribe({exchange: 'testExchange1', ack: true}, function(msg) {
+			assert.deepEqual(msg.content.toString(), 'Hello World');
+			exchangeMessageReceived	= true;
+		});
+
+		setTimeout(function() {
+			const	Intercom	= require(__dirname + '/../index.js').Intercom,
+				intercom	= new Intercom(require(confFile).default),
+				message	= 'Hello World';
+
+			intercom.send({que: 'testQue2', exchange: 'testExchange1'}, message);
+
+			intercom.consume({que: 'testQue2', ack: true}, function(msg) {
+				assert.deepEqual(msg.content.toString(), 'Hello World');
+				assert.deepEqual(exchangeMessageReceived, true);
+				done();
+			});
+		}, 500);
+	});
+
+
+	it('Subscribe & Publish', function(done) {
+		const	Intercom	= require(__dirname + '/../index.js').Intercom,
+			intercom	= new Intercom(require(confFile).default);
+
+		intercom.subscribe({exchange: 'testExchange2', ack: true}, function(msg) {
+			assert.deepEqual(msg.content.toString(), 'Hello World');
+			done();
+		});
+
+		setTimeout(function() {
+			intercom.publish({exchange: 'testExchange2'}, 'Hello World');
+		}, 500);
+	});
+
 });
